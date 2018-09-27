96e1bfce29374ee19029a12d06759e77713efbbe
magic.loveptit@gmail.com <magic.loveptit@gmail.com>
2018-09-27 14:19:01 +0700

    2

--- app/modules/cron/controllers/cron.php
+++ app/modules/cron/controllers/cron.php
@@ -8,50 +8,6 @@ class cron extends MX_Controller {
 		$this->load->model(get_class($this).'_model', 'model');
 	}
 	
-	
-	public function test_email (){
-//	require_once "vendor/autoload.php";
-
-require_once APPPATH.'libraries/PHPMailer/class.PHPMailer.php';
-        $mail = new PHPMailer;
-        echo "2222";
-        //Enable SMTP debugging. 
-        $mail->SMTPDebug = 3;                               
-        //Set PHPMailer to use SMTP.
-        $mail->isSMTP();            
-        //Set SMTP host name                          
-        $mail->Host = "smtp.gmail.com";
-        //Set this to true if SMTP host requires authentication to send email
-        $mail->SMTPAuth = true;                          
-        //Provide username and password     
-        $mail->Username = "zinbee.automation@gmail.com";                 
-        $mail->Password = "Muaha2016";                           
-        //If SMTP requires TLS encryption then set it
-        $mail->SMTPSecure = "tls";                           
-        //Set TCP port to connect to 
-        $mail->Port = 587;                                   
-        
-        $mail->From = "zinbee.automatio@gmail.com";
-        $mail->FromName = "Zinbee";
-        
-        $mail->addAddress("Toantq1908@gmail.com", "Recepient Name");
-        
-        $mail->isHTML(true);
-        
-        $mail->Subject = "Subject Text";
-        $mail->Body = "<i>Mail body in HTML</i>";
-        $mail->AltBody = "This is the plain text version of the email content";
-         echo "33";
-        if(!$mail->send()) 
-        {
-            echo "Mailer Error: " . $mail->ErrorInfo;
-        } 
-        else 
-        {
-            echo "Message has been sent successfully";
-        }
-	}
-	
 	public function  test4 (){
 	    echo '<pre>';
 	    $id = "46";
